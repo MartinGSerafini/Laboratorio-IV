6235ab6135cc4ccbc6d67ce8dd1e9f4a5f334290
Martin guillermo serafini <guillermoserafini61@gmail.com>
2025-03-31 13:09:32 -0300

    .

--- TP N1 Grupo 2/src/Dominio/Empleado.java
+++ TP N1 Grupo 2/src/Dominio/Empleado.java
@@ -1,80 +1,56 @@
 package Dominio;
 
 public class Empleado {
-	
-	private static int cont = 1000;  
-	
-	///Atributos
-	
-	private int id;
-	private String nombre;
-	private int edad;  
-	
-	///Constructores
-	
-	public Empleado(){
-<<<<<<< HEAD:TP N1 Grupo 2/src/Dominio/Empleado.java
-		this.id = cont++;
-		nombre  = "Sin Nombre";
-		edad = 99;
-	}
-	public Empleado(String nombre, int edad){
-		this.id = cont++;
-		this.nombre  = nombre;
-		this.edad = edad;
-=======
-		cont++; 
-		Id = cont;
-		Nombre  = "Sin Nombre";
-		Edad = 99;
-	}
-	public Empleado(String Nombre, int Edad){
-		cont++; 
-		this.Id = cont;
-		this.Nombre  = Nombre;
-		this.Edad = Edad;
->>>>>>> 18589bbfb234be0b0a6da89d58a51d4f022da919:TP N1 Grupo 2.v3/src/Dominio/Empleado.java
-	}
-	
-	
-	///Getters y Setters
-	
-	public int getId() {
-		return id;
-	}
-	public String getNombre() {
-		return nombre;
-	}
-	public void setNombre(String nombre) {
-		this.nombre = nombre;
-	}
-	public int getEdad() {
-		return edad;
-	}
-	public void setEdad(int edad) {
-<<<<<<< HEAD:TP N1 Grupo 2/src/Dominio/Empleado.java
-		this.edad = edad;
-=======
-		Edad = edad;
->>>>>>> 18589bbfb234be0b0a6da89d58a51d4f022da919:TP N1 Grupo 2.v3/src/Dominio/Empleado.java
-	}
-	
-	///Metodos
-	
-	public static int devuelveProximoID() {
-<<<<<<< HEAD:TP N1 Grupo 2/src/Dominio/Empleado.java
-        return cont;
+
+    private static int cont = 1000;  
+
+    // Atributos
+    private final int id;  // El ID es constante y no se debe cambiar
+    private String nombre;
+    private int edad;  
+
+    // Constructores
+    public Empleado(){
+        this.id = cont++;  // Asigna el ID y luego incrementa el contador
+        this.nombre = "Sin Nombre";
+        this.edad = 99;
     }
-	
-	///metodo toString()
-	@Override
-	public String toString() {
-		 return "Empleado: " + nombre + ", Edad: " + edad + ", Legajo: " + id;
+
+    public Empleado(String nombre, int edad){
+        this.id = cont++;  // Asigna el ID y luego incrementa el contador
+        this.nombre = nombre;
+        this.edad = edad;
     }
-=======
-		return cont+1;
-	}
-	
->>>>>>> 18589bbfb234be0b0a6da89d58a51d4f022da919:TP N1 Grupo 2.v3/src/Dominio/Empleado.java
-	
+
+    // Getters y Setters
+    public int getId() {
+        return id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    // Métodos
+    public static int devuelveProximoID() {
+        return cont;  // Devuelve el próximo ID disponible
+    }
+
+    // Método toString()
+    @Override
+    public String toString() {
+        return "Empleado: " + nombre + ", Edad: " + edad + ", Legajo: " + id;
+    }
 }
--- TP N1 Grupo 2/src/Dominio/Principal.java
+++ TP N1 Grupo 2/src/Dominio/Principal.java
@@ -2,27 +2,23 @@ package Dominio;
 
 public class Principal {
 
-	public static void main(String[] args) {
-		
-<<<<<<< HEAD:TP N1 Grupo 2/src/Dominio/Principal.java
-		Empleado emp1 = new Empleado();
-		System.out.println(emp1.toString());
-		
-		Empleado emp2 = new Empleado("Patricio", 36);
-		System.out.println(emp2.toString());
-		
-		Empleado emp3 = new Empleado("Maria", 35);
-		System.out.println(emp3.toString());
-		
-		Empleado emp4 = new Empleado();
-		System.out.println(emp4.toString());
-		
-		Empleado emp5 = new Empleado("Renee", 29);
-		System.out.println(emp5.toString());
-		
-		System.out.println("El proximo ID sera el " + Empleado.devuelveProximoID());
-=======
->>>>>>> 18589bbfb234be0b0a6da89d58a51d4f022da919:TP N1 Grupo 2.v3/src/Dominio/Principal.java
-	}
-
+    public static void main(String[] args) {
+        
+        Empleado emp1 = new Empleado();
+        System.out.println(emp1.toString());
+        
+        Empleado emp2 = new Empleado("Patricio", 36);
+        System.out.println(emp2.toString());
+        
+        Empleado emp3 = new Empleado("Maria", 35);
+        System.out.println(emp3.toString());
+        
+        Empleado emp4 = new Empleado();
+        System.out.println(emp4.toString());
+        
+        Empleado emp5 = new Empleado("Renee", 29);
+        System.out.println(emp5.toString());
+        
+        System.out.println("El próximo ID será el " + Empleado.devuelveProximoID());
+    }
 }
