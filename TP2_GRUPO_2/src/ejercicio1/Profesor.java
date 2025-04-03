ac5b7720b003aadad55fbb4073511c9e6a711d4f
Nicolas <mosovich.nicolas@gmail.com>
2025-04-03 12:52:08 -0300

    Constructor y ToString Profesor

--- TP2_GRUPO_2/src/ejercicio1/Profesor.java
+++ TP2_GRUPO_2/src/ejercicio1/Profesor.java
@@ -1,6 +1,48 @@
 package ejercicio1;
 
 public class Profesor extends Empleado {
-     String cargo;
-     int antiguedadDocente;
+     
+	//Atributos
+	private String cargo;
+    private int antiguedadDocente;
+	
+    //Constructores
+    
+    //	Vacio
+    public Profesor() {
+		super();
+		this.cargo = "Sin cargo";
+		this.antiguedadDocente = 0;
+	}
+    //	Con parámetros
+	public Profesor(String nombre, int edad, String cargo, int antiguedadDocente) {
+		super(nombre, edad);
+		this.cargo = cargo;
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	
+	//Getters y setters
+	public String getCargo() {
+		return cargo;
+	}
+	public void setCargo(String cargo) {
+		this.cargo = cargo;
+	}
+	public int getAntiguedadDocente() {
+		return antiguedadDocente;
+	}
+	public void setAntiguedadDocente(int antiguedadDocente) {
+		this.antiguedadDocente = antiguedadDocente;
+	}
+	
+	@Override
+	public String toString() {
+		return super.toString()+" Cargo: " + cargo + ", Antiguedad docente: " + antiguedadDocente;
+	}
+	
+	
+	
+	
+	
+	
 }
