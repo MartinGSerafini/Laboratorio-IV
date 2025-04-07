ab3de3a5502a884f9bec1329d0173cd66bb9cea3
Chantal Coronel <109154685+coronelChantal@users.noreply.github.com>
2025-04-07 16:03:10 -0300

    productoFresco y temperaturaRecomendada

--- TP2_GRUPO_2/src/ejercicio2/ProductoCongelado.java
+++ TP2_GRUPO_2/src/ejercicio2/ProductoCongelado.java
@@ -4,7 +4,8 @@ public class ProductoCongelado extends Producto{
 	//Atributos
 		private String tipoProducto;
 	    private int mesesAntesCaducidad;
-		
+	    private float temperaturaRecomendada;
+	    
 	    //Constructores
 	    
 	    //	Vacio
@@ -12,12 +13,14 @@ public class ProductoCongelado extends Producto{
 			super();
 			this.tipoProducto = "Sin Ingresar";
 			this.mesesAntesCaducidad = 0;
+			this.temperaturaRecomendada = 0;
 		}
 	    //	Con parametros
-		public ProductoCongelado(String nombre, String tipoProducto, int mesesAntesCaducidad) {
+		public ProductoCongelado(String nombre, String tipoProducto, int mesesAntesCaducidad, float temperaturaRecomendada) {
 			super(nombre);
 			this.tipoProducto = tipoProducto;
 			this.mesesAntesCaducidad = mesesAntesCaducidad;
+			this.temperaturaRecomendada = temperaturaRecomendada;
 		}
 		
 		// Getters and Setters
@@ -33,5 +36,13 @@ public class ProductoCongelado extends Producto{
 		public void setMesesAntesCaducidad(int mesesAntesCaducidad) {
 			this.mesesAntesCaducidad = mesesAntesCaducidad;
 		}
+		public float getTemperaturaRecomendada() {
+			return temperaturaRecomendada;
+		}
+		public void setTemperaturaRecomendada(float temperaturaRecomendada) {
+			this.temperaturaRecomendada = temperaturaRecomendada;
+		}
+		
+		
 		
 }
--- TP2_GRUPO_2/src/ejercicio2/ProductoFresco.java
+++ TP2_GRUPO_2/src/ejercicio2/ProductoFresco.java
@@ -1,5 +1,35 @@
 package ejercicio2;
 
 public class ProductoFresco extends Producto{
-
+	private String fechaEnvasado;
+	private String paisOrigen; 
+	
+	// Constructores
+	//vacio
+	public ProductoFresco() {
+		super();
+		this.fechaEnvasado = "Sin Ingresar";
+		this.paisOrigen = "Sin Ingresar";
+	}
+	//con parametros
+	public ProductoFresco(String nombre, String fechaEnvasado, String paisOrigen) {
+		super(nombre);
+		this.fechaEnvasado = fechaEnvasado;
+		this.paisOrigen = paisOrigen;
+	}
+	
+	//getters and setters
+	public String getFechaEnvasado() {
+		return fechaEnvasado;
+	}
+	public void setFechaEnvasado(String fechaEnvasado) {
+		this.fechaEnvasado = fechaEnvasado;
+	}
+	public String getPaisOrigen() {
+		return paisOrigen;
+	}
+	public void setPaisOrigen(String paisOrigen) {
+		this.paisOrigen = paisOrigen;
+	}
+	
 }
