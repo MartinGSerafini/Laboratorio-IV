5f7bf4e6b387738e6856368281d8604244440564
Chantal Coronel <109154685+coronelChantal@users.noreply.github.com>
2025-04-07 20:52:44 -0300

    inicio punto 3

---
+++ TP2_GRUPO_2/src/ejercicio3/EdificioDeOficinas.java
@@ -0,0 +1,11 @@
+package ejercicio3;
+
+public class EdificioDeOficinas implements Edificio{
+
+	@Override
+	public double getSuperficieEdificio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
