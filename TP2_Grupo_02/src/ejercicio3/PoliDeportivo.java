241313c47fcbbc1c70983427045c7d357598d246
Martin guillermo serafini <guillermoserafini61@gmail.com>
2025-04-08 14:21:49 -0300

    .

---
+++ TP2_Grupo_02/src/ejercicio3/PoliDeportivo.java
@@ -0,0 +1,18 @@
+package ejercicio3;
+
+public class PoliDeportivo implements Edificio, InstalacionDeportiva {
+
+	@Override
+	public int getTipoInstalacion() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public double getSuperficieEdificio() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+	
+
+}
