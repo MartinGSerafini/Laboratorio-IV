08483dd779727de92192d40bec77a3bd61a9e58e
Martin guillermo serafini <guillermoserafini61@gmail.com>
2025-04-08 22:15:45 -0300

    Cree el main y arregle el nombre del prouyecto

---
+++ TP2_Grupo_02/src/ejercicio3/mainEjercicio3.java
@@ -0,0 +1,12 @@
+package ejercicio3;
+
+import java.util.ArrayList;
+
+public class mainEjercicio3 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 ArrayList<String> Lista = new ArrayList<>();
+	}
+
+}
