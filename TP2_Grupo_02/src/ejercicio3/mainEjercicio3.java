9bf8abda7ec35ed481efda3954315d3308c0642e
Vicky-2706 <vickyelizabethsantacruz@gmail.com>
2025-04-08 23:08:01 -0300

    .

--- TP2_Grupo_02/src/ejercicio3/mainEjercicio3.java
+++ TP2_Grupo_02/src/ejercicio3/mainEjercicio3.java
@@ -7,7 +7,7 @@ public class mainEjercicio3 {
 	public static void main(String[] args) {
 		 ArrayList<PoliDeportivo> Lista = new ArrayList<PoliDeportivo>();
 		 Lista.add(new PoliDeportivo("Polideportivo Tigre", 1500.6, 1));
-		 Lista.add(new PoliDeportivo("Polideportivo Pacheco", 1100.7, 1));
-		 Lista.add(new PoliDeportivo("Polideportivo El Talar", 1300.8, 1));
+		 Lista.add(new PoliDeportivo("Polideportivo Pacheco", 1100.7, 2));
+		 Lista.add(new PoliDeportivo("Polideportivo El Talar", 1300.8, 3));
 	}
 }
