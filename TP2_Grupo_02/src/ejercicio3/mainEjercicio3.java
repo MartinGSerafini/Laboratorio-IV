d8ba3eaa98efd3f1b76a0faa12ead95e8cafde73
Pilar <sofiapilarvitalevi@gmail.com>
2025-04-09 08:42:30 -0300

    Punto 3 main

--- TP2_Grupo_02/src/ejercicio3/mainEjercicio3.java
+++ TP2_Grupo_02/src/ejercicio3/mainEjercicio3.java
@@ -1,13 +1,19 @@
 package ejercicio3;
 
 import java.util.ArrayList;
+import java.util.ListIterator;
+
+import ejercicio1.Profesor;
 
 public class mainEjercicio3 {
 
 	public static void main(String[] args) {
-		 ArrayList<PoliDeportivo> Lista = new ArrayList<PoliDeportivo>();
+		 ArrayList<Edificio> Lista = new ArrayList<Edificio>();
 		 Lista.add(new PoliDeportivo("Polideportivo Tigre", 1500.6, 1));
 		 Lista.add(new PoliDeportivo("Polideportivo Pacheco", 1100.7, 2));
 		 Lista.add(new PoliDeportivo("Polideportivo El Talar", 1300.8, 3));
+		 Lista.add(new EdificioDeOficinas(50, 30));
+		 Lista.add(new EdificioDeOficinas(65, 40));
+		 
 	}
 }
