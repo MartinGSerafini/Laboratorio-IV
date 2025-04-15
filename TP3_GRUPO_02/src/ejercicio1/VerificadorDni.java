6297d0dce5f7d7955686d303a949499c49ddac7f
Vicky-2706 <vickyelizabethsantacruz@gmail.com>
2025-04-15 18:12:39 -0300

    Update VerificadorDni.java

--- TP3_GRUPO_02/src/ejercicio1/VerificadorDni.java
+++ TP3_GRUPO_02/src/ejercicio1/VerificadorDni.java
@@ -4,6 +4,15 @@ public class VerificadorDni {
 
 	public static void verificarDniInvalido(String dni) throws DniInvalidoException {
 		
+		for(int i=0; i<dni.length(); i++) {
+			char c = dni.charAt(i);
+			if(Character.isLetter(c)) {
+				// EL DNI CONTIENE UNA LETRA
+			}
+			else {
+				// EL DNI SOLO CONTIENE NUMEROS
+			}
+		}
 		
 	}
 }
