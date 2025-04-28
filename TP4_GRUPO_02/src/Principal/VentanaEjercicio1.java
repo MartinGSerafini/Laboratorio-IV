c8eee024406284ea8a5a853cf31a1ad324aca071
Martin guillermo serafini <guillermoserafini61@gmail.com>
2025-04-28 08:23:34 -0300

    .

--- TP4_GRUPO_02/src/Principal/VentanaEjercicio1.java
+++ TP4_GRUPO_02/src/Principal/VentanaEjercicio1.java
@@ -3,16 +3,20 @@ package Principal;
 import java.awt.BorderLayout;
 import java.awt.Font;
 import java.awt.GridLayout;
-
 import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import javax.swing.JTextField;
 
 public class VentanaEjercicio1 extends JFrame {
 	
-private JPanel contentPane;
+	private JPanel contentPane;
+	private JTextField nombreField, telefonoField, emailField, fechaField;
+	private JLabel resultadoLabel;
 	
 	public VentanaEjercicio1() {
 		
@@ -28,16 +32,42 @@ private JPanel contentPane;
 		contentPane.add(panel, BorderLayout.NORTH);
 		panel.setLayout(new GridLayout(0, 1, 0, 0));
 		
-		JLabel lblNewLabel = new JLabel("");
-		panel.add(lblNewLabel);
-		
-		JLabel lblNewLabel_1 = new JLabel("      Ejercicio 1");
-		panel.add(lblNewLabel_1);
-		lblNewLabel_1.setFont(new Font("Arial", Font.BOLD, 17));
-		
 		JPanel panel_1 = new JPanel();
 		contentPane.add(panel_1, BorderLayout.CENTER);
 		panel_1.setLayout(null);
 		
+		setTitle("Contactos"); //Cambia el titolo de la ventana
+		
+		JLabel lblNombre = new JLabel("Nombre:");
+		lblNombre.setBounds(50, 20, 100, 25);
+		panel_1.add(lblNombre);
+
+		nombreField = new JTextField();
+		nombreField.setBounds(150, 20, 200, 25);
+		panel_1.add(nombreField);
+
+		JLabel lblTelefono = new JLabel("Teléfono:");
+		lblTelefono.setBounds(50, 60, 100, 25);
+		panel_1.add(lblTelefono);
+
+		telefonoField = new JTextField();
+		telefonoField.setBounds(150, 60, 200, 25);
+		panel_1.add(telefonoField);
+
+		JLabel lblEmail = new JLabel("Email:");
+		lblEmail.setBounds(50, 100, 100, 25);
+		panel_1.add(lblEmail);
+
+		emailField = new JTextField();
+		emailField.setBounds(150, 100, 200, 25);
+		panel_1.add(emailField);
+		
+		JLabel lblFecha = new JLabel("Fecha Nac:");
+		lblFecha.setBounds(50, 140, 100, 25);
+		panel_1.add(lblFecha);
+
+		fechaField = new JTextField();
+		fechaField.setBounds(150, 140, 200, 25);
+		panel_1.add(fechaField);
 	}
 }
