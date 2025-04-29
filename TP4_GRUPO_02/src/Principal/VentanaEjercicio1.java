a72a8f79bb0c3a6b5040b463099e5321036f4eaf
Chantal Coronel <109154685+coronelChantal@users.noreply.github.com>
2025-04-29 14:55:46 -0300

    .

--- TP4_GRUPO_02/src/Principal/VentanaEjercicio1.java
+++ TP4_GRUPO_02/src/Principal/VentanaEjercicio1.java
@@ -21,7 +21,7 @@ public class VentanaEjercicio1 extends JFrame {
 	
 	public VentanaEjercicio1() {
 		
-		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		setBounds(100, 100, 450, 300);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
